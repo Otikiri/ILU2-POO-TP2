d4c9b26b01cd0fc72bf53a0db9b575c3a569b9c3
Otikiri <sayvirakyuth@gmail.com>
2025-03-18 21:25:34 +0100

    cas 2.7 termine

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,16 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] donneMarche = controlAfficherMarche.donnerInfoMarche();
+		if (donneMarche.length==0) {
+			System.out.println("le marche est vide revenez plus tard");
+		}
+		else {
+			System.out.println(nomAcheteur+", vous trouverez au marche : ");
+			for (int i = 0; i < donneMarche.length; i+=3) {
+				System.out.println("-"+donneMarche[i]+" qui vend "+donneMarche[i+1]+" "+donneMarche[i+2]);
+			}
+		}
 	}
 }
+		
