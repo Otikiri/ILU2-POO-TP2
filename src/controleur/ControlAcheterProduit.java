d56b58474039738556c4fd390ac1d3281eba072c
Otikiri <sayvirakyuth@gmail.com>
2025-03-19 12:00:04 +0100

    cas2.8 a terminer

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,5 +1,6 @@
 package controleur;
 
+import villagegaulois.Etal;
 import villagegaulois.Village;
 
 public class ControlAcheterProduit {
@@ -14,6 +15,35 @@ public class ControlAcheterProduit {
 		this.controlVerifierIdentite = controlVerifierIdentite;
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
-
-	//TODO a completer
+	
+	
+	public String[] listeVendeur() {
+		String[] donneMarche = village.donnerEtatMarche();
+		int sizeliste = village.donnerNbEtal();
+		String[] listeV = new String[sizeliste];
+		int pos = 0; 
+		for (int i = 0; i < donneMarche.length; i+=3) {
+			listeV[pos] = donneMarche[i];
+			pos++;
+			
+		}
+		return listeV;
+	}
+	
+	public int acheterProduit(String nomVendeur,int vendu) {
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		int nbProduit = etal.getQuantite();
+		if (nbProduit==0) {
+			return -1;
+		}
+		if (nbProduit<vendu) {
+			int venduR = vendu-nbProduit;
+			etal.acheterProduit(venduR);
+			return nbProduit;
+		}
+		else {
+			etal.acheterProduit(vendu);
+			return 0;
+		}
+	}
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -1,15 +1,47 @@
 package frontiere;
 
+
 import controleur.ControlAcheterProduit;
+import controleur.ControlVerifierIdentite;
 
 public class BoundaryAcheterProduit {
 	private ControlAcheterProduit controlAcheterProduit;
-
-	public BoundaryAcheterProduit(ControlAcheterProduit controlAcheterProduit) {
+	private ControlVerifierIdentite controlVerifierIdentite;
+	public BoundaryAcheterProduit(ControlAcheterProduit controlAcheterProduit,ControlVerifierIdentite controlVerifierIdentite) {
 		this.controlAcheterProduit = controlAcheterProduit;
+		this.controlVerifierIdentite = controlVerifierIdentite;
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO à completer
+		if (!controlVerifierIdentite.verifierIdentite(nomAcheteur)) {
+			System.out.println("je suis desolee "+nomAcheteur+"...");
+		}
+		
+		String nomProduits = Clavier.entrerChaine("Quel produits voulez vous acheter?\n");
+		String[] listeV = controlAcheterProduit.listeVendeur();
+		int pos= 0;
+		for (int i = 0; i < listeV.length; i++) {
+			System.out.println((pos+1)+" - "+listeV[i]);
+			
+			
+		}
+		int idVendeur = Clavier.entrerEntier("Chez quel commercant voulez-vous acheter des "+nomProduits);
+		if (controlVerifierIdentite.verifierIdentite(listeV[idVendeur-1])) {
+			System.out.println("je suis desolee "+listeV[idVendeur-1]+"...");
+		}
+		
+		System.out.println(nomAcheteur+" se deplace jusqu'a l'etal du vendeur "+listeV[idVendeur-1]);
+		
+		int nbproduit = Clavier.entrerEntier("Combien de "+nomProduits+" voulez vous acheter?");
+		int retour = controlAcheterProduit.acheterProduit(nomProduits, nbproduit);
+		if (retour ==-1) {
+			System.out.println(nomAcheteur+"veut acheter "+nbproduit+" mais il n'y en a plus.");
+		}
+		if (retour>0) {
+			System.out.println(nomAcheteur+"veut acheter "+nbproduit+" mais"+listeV[idVendeur-1]+"n'a que "+retour+" il achete tt le stock");
+		}
+		if (retour==0) {
+			System.out.println(nomAcheteur+"veut acheter "+nbproduit+" "+nomProduits+" a "+listeV[idVendeur-1]);
+		}
 	}
 }
